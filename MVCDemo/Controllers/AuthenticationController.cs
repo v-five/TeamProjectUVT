746659ddfe546b635ac9fdab2d273b2c9fff0702
Mirica Ioana <ioana.mirica@bluecouchdigital.com>
2016-05-24 11:11:53 +0300

    Fixes after the merge

--- MVCDemo/Controllers/AuthenticationController.cs
+++ MVCDemo/Controllers/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Database;
+using MVCDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,14 +11,49 @@ namespace MVCDemo.Controllers
     public class AuthenticationController : Controller
     {
         // GET: Register
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult Register()
         {
-            return RedirectToAction("Person", "Person");
+            var email = Request["email"];
+            var pass = Request["password"];
+            var cpass = Request["confirm-password"];
+            if(string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(pass))
+            {
+                return Redirect("/?error=emptyMailOrPassword");
+            }
+            if(pass != cpass)
+            {
+                return Redirect("/?error=passwordNotEqual");
+            }
+            var person = new Person
+            {
+                Auth = new Authentication
+                {
+                    Username = email,
+                    Password = pass
+                },
+                ContactInfo = new ContactInfo
+                {
+                    Email = email,
+                    PhoneNumbers = new List<string>(),
+                    Addresses = new List<Address>(),
+                    SocialWebSites = new List<SocialMedia>()
+                },
+                BasicInfo = new BasicInfo
+                {
+                    Birthday = DateTime.Now
+                },
+                ProfessionalInfo = new ProfessionalInfo
+                {
+                    ExpectedSalary = new Range(),
+                    Skills = new List<Skill>()
+                }
+            };
+
+            var context = new DemoContext();
+            context.Person.Add(person);
+            context.SaveChanges();
+
+            return Redirect("/People/EditPerson/" + person.Id);
         }
     }
 }
---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,70 @@
+﻿using MVCDemo.Models;
+using System.Web.Mvc;
+using Database;
+using System.Linq;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public ActionResult Index()
+        {
+            // TODO: view all persons
+            return View();
+        }
+
+        public ActionResult Person(int id)
+        {
+            var context = new DemoContext();
+            var person = context.Person
+                .Include("BasicInfo")
+                .Include("ContactInfo")
+                .Include("ProfessionalInfo")
+                .SingleOrDefault(p => p.Id == id);
+
+            // TODO: in view sa se populeze datele care exista
+            return View(person);
+        }
+        
+        [HttpGet]
+        public ActionResult EditPerson(int id)
+        {
+            var context = new DemoContext();
+            var person = context.Person
+                .Include("BasicInfo")
+                .Include("ContactInfo")
+                .Include("ProfessionalInfo")
+                .SingleOrDefault(p => p.Id == id);
+
+            // TODO: in view sa se populeze datele care exista
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult EditPerson(Person person)
+        {
+            //TODO: Tudor adauga logica de save person cu datele din formular
+            return Redirect("People/person/" + person.Id);
+        }
+        
+        [HttpDelete]
+        public string DeletePerson(int id)
+        {
+            var context = new DemoContext();
+            var itemToDelete = context.Person.Find(id);
+            if (itemToDelete != null)
+            {
+                itemToDelete.IsDeleted = true;
+                context.SaveChanges();
+                string message = "204";
+                return message;
+            }
+            else
+            {
+                string message = "404";
+                return message;
+            }
+        }
+    }
+}
+
