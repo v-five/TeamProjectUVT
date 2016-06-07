3d4f33e96cbe43a9e22391711c06a516a6af3abb
Kozma Monika <kozmamonika95@gmail.com>
2016-06-07 10:24:10 +0300

    Am adaugat logica pentru filtrare

--- MVCDemo/Controllers/PeopleController.cs
+++ MVCDemo/Controllers/PeopleController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using Database;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace MVCDemo.Controllers
 {
@@ -11,12 +12,26 @@ namespace MVCDemo.Controllers
         public ActionResult Index()
         {
             var context = new Database.DemoContext();
-            List<Person> allperson = context.Person
+            IEnumerable<Person> allperson = context.Person
                 .Include("BasicInfo")
                 .Include("ContactInfo")
                 .Include("ProfessionalInfo").ToList();
-        
-            return View(allperson);
+
+            int i;
+            if(int.TryParse(Request["yearsOfExperience"], out i))
+                allperson = allperson.Where(p => p.ProfessionalInfo.YearsOfExperience == i);
+            if(!string.IsNullOrWhiteSpace(Request["city"]))
+                allperson = allperson.Where(p => p.ContactInfo.Addresses.Any(a => a.City == Request["city"]));
+            if (!string.IsNullOrWhiteSpace(Request["lastDegreeEarned"]))
+                allperson = allperson.Where(p => p.ProfessionalInfo.LastDegreeEarned == Request["lastDegreeEarned"]);
+            if (int.TryParse(Request["currentSalary"], out i))
+                allperson = allperson.Where(p => p.ProfessionalInfo.CurrentSalary == i);
+            if (int.TryParse(Request["expectedSalary"], out i))
+                allperson = allperson.Where(p => (p.ProfessionalInfo.ExpectedSalary.Min) < i && (p.ProfessionalInfo.ExpectedSalary.Max > i));
+            if (int.TryParse(Request["age"], out i))
+                allperson = allperson.Where(p => (DateTime.Today.Year - p.BasicInfo.Birthday.Year) == i);
+            return View(allperson.ToList());
+  
         }
 
         public ActionResult Person(int id)
